292e4400c370d06f3a8a94fe82fb18ab79e37ddf
SleepyHardbass <142543301+SleepyHardbass@users.noreply.github.com>
2023-12-25 10:18:11 +0000

    Update hilbert.lua

--- hilbert.lua
+++ hilbert.lua
@@ -14,10 +14,10 @@ local function xy2d(n, x, y)
     n = SHR(n, 1)
     local rx, ry, d = 0, 0, 0
     while n > 0 do
-        -- BAND(x, n) > 0 and -1 or 0
-        rx = SAR(NOT(AND(x, n) - 1), 31) -- -1|0
-        -- BAND(y, n) > 0 and 1 or 0
-        ry = SHR(NOT(AND(y, n) - 1), 31) -- 1|0
+        -- AND(x, n) > 0 and -1 or 0
+        rx = SAR(-AND(x, n), 31) -- -1|0
+        -- AND(y, n) > 0 and 1 or 0
+        ry = SHR(-AND(y, n), 31) -- 1|0
         
         --[[ mbmb
             qn = n*n
@@ -30,7 +30,7 @@ local function xy2d(n, x, y)
             rx == 1 and ry == 1 --> 2*qn --> qn + qn
             rx == 1 and ry == 0 --> 3*qn --> qn + qn + qn
             
-            d = OR( d + AND(qn, -XOR(rx, ry)) + AND(SHL(qn, 1), -rx) )
+            d = OR( d + AND(qn, -XOR(rx, ry)) + AND(qn + qn, -rx) )
             
             qn = SHR(qn, 2)
         --]]
@@ -38,10 +38,10 @@ local function xy2d(n, x, y)
         
         -- if ry == 0 then XOR SWAP(x, y)
         -- -- if rx ~= 0 then x, y = NOT(x), NOT(y) 
-        x = XOR(x, AND(y, ry - 1) + AND(NOT(y + y), rx)) -- NOT(y + y) == 1 - y - y
+        x = XOR(x, AND(y, ry - 1) + AND(NOT(y + y), rx))
         y = XOR(y, AND(x, ry - 1))
         x = XOR(x, AND(y, ry - 1) + AND(NOT(y + y), rx))
-        n = SHR(n, 1) -- n = floor(n/2)
+        n = SHR(n, 1) -- floor(n/2)
     end
     return d
 end
@@ -52,7 +52,7 @@ local function d2xy(n, d)
     while s < n do
         rx = AND(1, SHR(t, 1)) -- 1|0
         ry = AND(1, XOR(t, rx)) -- 1|0
-        rx = NOT(rx - 1) -- UNM
+        rx = OR(-rx) -- NEG
         
         -- if ry == 0 then XOR SWAP(x, y)
         -- -- if rx ~= 0 then x, y = s + NOT(x), s + NOT(y) 
@@ -61,7 +61,7 @@ local function d2xy(n, d)
         x = XOR(x, AND(y, ry - 1) + AND(s + NOT(y + y), rx))
         x = OR(x + AND(s, rx)) -- x = x | x = x + s
         y = OR(y + AND(s, -ry)) -- y = y | y = y + s
-        s = SHL(s, 1) -- s = s*2
+        s = s + s -- SHL(s, 1) -- s*2
         t = SHR(t, 2) -- t = t/4
     end
     return x, y
